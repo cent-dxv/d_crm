423d0f734a2fc182d8e660cf26f3e70ce9f35c1a
cent-dxv <centdxv@gmail.com>
2023-05-08 13:51:39 +0300

    basick route

--- core_site/views.py
+++ core_site/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# # testing the index route
+# def index(response) :
+#     return HttpResponse("hola")
+
+def index(response) :
+        return render( response ,"index.html" , {}) 
