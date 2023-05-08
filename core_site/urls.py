5d99076710978a768c1737fc530b39286531a8aa
cent-dxv <centdxv@gmail.com>
2023-05-08 17:37:04 +0300

    basic log in/out done

--- core_site/urls.py
+++ core_site/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='home'),
+    path('login',views.login_user, name='login_user'),
+    # path('logout',views.logout_user, name='logout_user'),
+    path('register',views.register, name='register'),
 ]
--- core_site/views.py
+++ core_site/views.py
@@ -1,11 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 # # testing the index route
 # def index(response) :
 #     return HttpResponse("hola")
 
-def index(response) :
-        return render( response ,"index.html" , {}) 
+
+def index(response):
+    return render(response, "home.html", {})
+
+
+def login_user(request):
+    print(request.POST , request.POST.get("log") , "l \n\n")
+
+    if request.method == "POST" and request.POST.get("log") == "log_out":
+        logout(request)
+        print("log out")
+        return redirect('home')
+
+    elif request.method == "POST":
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request ,username= username,password  =password)
+        print(user)
+        if user :
+            login(request, user)
+            messages.success(request , "you successfully signed")
+            return redirect('home')
+        else :
+            if username : messages.error(request , "user name or password incorect ")
+    return render(request, "login.html", {})
+
+
+def logout_user(request):
+    return render(request, "login.html", {})
+
+def register(request):
+    return render(request, "register.html")
